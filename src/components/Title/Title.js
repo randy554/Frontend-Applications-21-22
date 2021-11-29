d85d5ddd659a79216dc13eff7bcc31975457c93f
randy554 <randy554@hotmail.com>
2021-11-29 02:27:45 +0100

    useEffect() toegepast

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Show title on page
 const Title = () => {
   const [topic, setTopic] = useState("corona");
 
-  console.log("This is the topic:", topic);
+  useEffect(() => {
+    console.log(`It's ${topic} around here!`);
+  }, [topic]);
+
+  /* 
+   |  ---------------------------------------------------------------------------------------
+   |  The second argument in the useEffect function is a dependency array
+   |  If u pass an empty array the props & state will always have the initial values, 
+   |  as a result of that the effect function will not be called again.
+   |  
+   |  By passing a state value in the dependency array, it will check to see if the state 
+   |  has changed, if that is the case then the effect function will be called again.
+   |  
+   |  ---------------------------------------------------------------------------------------
+  */
 
   return (
     <>
@@ -16,9 +30,10 @@ const Title = () => {
         type="button"
         onClick={() => {
           // If the text field is not empty then take the value and set it as the new topic
-          if (document.querySelector("#TopicInput").value) {
-            setTopic(document.querySelector("#TopicInput").value);
-          }
+          // if (document.querySelector("#TopicInput").value) {
+          //   setTopic(document.querySelector("#TopicInput").value);
+          // }
+          setTopic(document.querySelector("#TopicInput").value);
         }}
       >
         Change
