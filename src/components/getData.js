37376321463367f6a057a2f9292fff71fdb09526
randy554 <randy554@hotmail.com>
2021-12-02 16:58:44 +0100

    Update getData.js

--- src/components/getData.js
+++ src/components/getData.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import newsData from "../data/newsData.json";
 
+// Used update function as base https://codesandbox.io/s/hva-demo-dag3-tnn6g?file=/src/hooks/useD3.js
 // Get the news data
 const GetData = () => {
   const [data, setData] = useState(null);
