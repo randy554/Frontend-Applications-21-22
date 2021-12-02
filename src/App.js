a50750a843f2a13298590d5773271b47f0964ba9
randy554 <randy554@hotmail.com>
2021-12-02 02:56:08 +0100

    removed contact page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Pages/Home/Home";
 import About from "./components/Pages/About/About";
-import Contact from "./components/Pages/Contact/Contact";
 import Whoops404 from "./components/Pages/Whoops404/Whoops404";
 import Navigation from "./components/Navigation/Navigation";
 
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Whoops404 />} />
         </Routes>
         <Navigation />
